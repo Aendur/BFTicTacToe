93d430715c1d526f7dc67e56287a2badb0766c2a
Aendur <Aendur@users.noreply.github.com>
2021-05-13 23:03:20 -0300

    refactor eco -> bfttt + npm script

--- src/bfttt/BFTTTServer.java
+++ src/bfttt/BFTTTServer.java
@@ -8,43 +8,100 @@ package bfttt;
 import bftsmart.tom.MessageContext;
 import bftsmart.tom.ServiceReplica;
 import bftsmart.tom.server.defaultservices.DefaultSingleRecoverable;
-import java.io.File;
-import java.io.IOException;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class BFTTTServer extends DefaultSingleRecoverable{
 
-	private int c = 0;
-	private int id;
-	private String dbPath;
-	public BFTTTServer (int id) {
-		this.id = id;
-		this.dbPath = "serverdata" + this.id + ".dat";
-		this.LoadDatabase();
-		new ServiceReplica(this.id,this,this);
+	//private int c = 0;
+	private JSONObject gameState = setInitialState();
+
+	private JSONObject setInitialState() {
+		JSONObject initialState = new JSONObject();
+		JSONArray board = new JSONArray();
+		board.put(0);
+		board.put(0);
+		board.put(0);
+		board.put(0);
+		board.put(0);
+		board.put(0);
+		board.put(0);
+		board.put(0);
+		board.put(0);
+		initialState.put("board", board);
+		initialState.put("status", "waitingForPLayers");
+		initialState.put("turn", "player0");
+		return initialState;
 	}
 
-	private void LoadDatabase() {
-		File dbFile = new File(this.dbPath);
-		if (dbFile.exists()) {
-			// load this file
-			System.out.println("Found DB file " + this.dbPath);
-		} else {
-			// create a new db file if it does not exist;
-			System.out.println("Creating a new DB file " + this.dbPath);
-			try {
-				dbFile.createNewFile();
-			} catch (IOException e) {
-				System.out.println("Failed to create DB file " + this.dbPath);
-			}
-		}
+	private void resetGameState() {
+		JSONObject initialState = new JSONObject();
+		JSONArray board = new JSONArray();
+		board.put(0);
+		board.put(0);
+		board.put(0);
+		board.put(0);
+		board.put(0);
+		board.put(0);
+		board.put(0);
+		board.put(0);
+		board.put(0);
+		initialState.put("board", board);
+		initialState.put("status", "waitingForPLayers");
+		initialState.put("turn", "player0");
+		gameState = initialState;
+	}
+
+	public BFTTTServer(int id) {
+		new ServiceReplica(id,this,this);
 	}
 
 	@Override
 	public byte[] appExecuteOrdered(byte[] bytes, MessageContext mc) {
 		String request = new String(bytes);
-		c++;
-		System.out.println("Recebeu requisição "+c+": "+request);
-		return ("Resposta "+c+" servidor: "+request).getBytes();
+		String name = new String();
+
+		System.out.println("Recebeu requisição: " + request);
+
+		try {
+			JSONObject resp = new JSONObject(new String(request));
+			String clientId = resp.getString("clientId");
+			switch(resp.getInt("action")) {
+				case 0:
+					System.out.println("Acao = registrar nome e time");
+					name = resp.getString("name");
+					if(!gameState.has("player0")) gameState.put("player0", clientId);
+					else if(!gameState.has("player1")) gameState.put("player1", clientId);
+					else return ("dc").getBytes();
+					System.out.println("Tabuleiro: "+ gameState.getJSONArray("board"));
+					break;
+				case 1:
+					System.out.println("Acao = marcar posicao");
+					name = resp.getString("name");
+					JSONArray board = gameState.getJSONArray("board");
+					int pos = resp.getInt("pos");
+					board.put(pos, 1);
+					System.out.println("Tabuleiro: "+ gameState.getJSONArray("board"));
+					break;
+				case 2:
+					System.out.println("Acao = jogador desconectado");
+					System.out.println("ID:" + clientId);
+					resetGameState();
+				case 3:
+					System.out.println("Acao = sincronizar");
+					break;
+				default:
+					System.out.println("Acao nao reconhecida");
+					break;
+			}
+		} catch (JSONException e) {
+			System.out.println("Erro: Nao foi possivel entender a pergunta do cliente");
+		}
+
+		//return ("Resposta servidor: " + request).getBytes();
+		return (gameState.toString()).getBytes();
 	}
 
 	@Override
@@ -54,20 +111,24 @@ public class BFTTTServer extends DefaultSingleRecoverable{
 
 	@Override
 	public byte[] getSnapshot() {
-		return Integer.toString(c).getBytes();
+		//return Integer.toString(c).getBytes();
+		return gameState.toString().getBytes();
 	}
 
 	@Override
 	public void installSnapshot(byte[] bytes) {
-		c = Integer.parseInt(new String(bytes));
+		//c = Integer.parseInt(new String(bytes));
+		JSONObject gameState = new JSONObject();
+		gameState.put("board", new int[] {0, 0, 0, 0, 0, 0, 0, 0 ,0});
+		System.out.println("FDSAFDSA: "+gameState.toString());
 	}
 
 	/**
-	* @param args the command line arguments
-	*/
+	 * @param args the command line arguments
+	 */
 	public static void main(String[] args) {
 		// TODO code application logic here
 		new BFTTTServer(Integer.parseInt(args[0]));
 	}
-	
+
 }
